af99a6f8886e6fc54efbb7b562968c1af725487d
jesseyproctor <jss.proctor@gmail.com>
2020-11-20 10:35:18 -0500

    note-text-function

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const url = 'http://localhost:3000/notes/'
 
+document
+    .querySelector('#save-note')
+    .addEventListener('click', function() {
+        const noteText = document.querySelector('#write-note').value
+        createNotes(noteText)
+    })
+
 function createNotes (noteText) { 
     fetch(url, {
         method: 'POST', 
         headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({title: "notes",
-            body: noteText})
+        body: JSON.stringify({
+            item: noteText
+           })
     })
         .then(res => res.json())
         .then(data => console.log(data))
 }
 
-let noteText = document.querySelector('#write-note')
-createNotes()
+
+
 
 
 
